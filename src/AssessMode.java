d4fdce20bca92706a3575527ef3e7583ae399372
MegaDarken <joe.swales@googlemail.com>
2021-01-27 18:32:50 +0000

    AssessMode assesses argument

--- src/AssessMode.java
+++ src/AssessMode.java
@@ -2,9 +2,18 @@
 
 class AssessMode
 {
-   
+   //Constants
+   const String ARGUMENT_SERVER = "-server";
+
+   const String PROGRAM_NAME;
+
    public static void main(String[] args)
    {
+      System.out.println("Starting " + );
+      
+      //Determine running mode
+      boolean asServer = Arrays.asList(args).contains(ARGUMENT_SERVER);
+      
       
    }
 }
