32b09db1551c2401fb45b741183e93fc5cc8e2c9
MegaDarken <joe.swales@googlemail.com>
2021-02-04 09:55:43 +0000

    Impliemented inital structure for NetworkConnection.java

--- src/NetworkConnection.java
+++ src/NetworkConnection.java
@@ -1,6 +1,20 @@
 //File: NetworkConnection.java
+import java.net.*;
 
 class NetworkConnection
 {
-
+   public InetAddress host = null;
+   
+   public NetworkConnection()
+   {
+      //Connection type to server/client?
+      try
+      {
+      
+      }
+      catch(UnknownHostException e)
+      {
+         System.out.println("ERR: Unable to resolve name and address");
+      }
+   }
 }
