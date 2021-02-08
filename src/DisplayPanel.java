0532c35317de7e5d863510315a4884949a8b1cdf
MegaDarken <joe.swales@googlemail.com>
2021-02-08 22:05:52 +0000

    DisplayPanel Added formating with frame

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -7,12 +7,23 @@ public class DisplayPanel extends JPanel implements ActionListener
 {
    private final int maximumImageLabels
    
+   private final int windowStartingPositionX = 640;
+   private final int windowStartingPositionY = 480;
+   private final int windowStartingSizeX = 640;
+   private final int windowStartingSizeY = 480;
+   
    private JLabel[] imageLabels;
    private Timer timer;
    
    public DisplayPanel()
    {
-   
+        setLayout(null);//Disable Grid
+        
+        JFrame frame = new JFrame();
+        dp.setBounds(windowStartingPositionX, windowStartingPositionY, windowStartSizeX, windowStartSizeY);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        
+        //Keymap 
    }
    
    
