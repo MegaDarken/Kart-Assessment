bb9eac5396ff44464fef140e46e46b81ca96c569
MegaDarken <joe.swales@googlemail.com>
2021-02-10 14:55:33 +0000

    RaceKart added Constants for Directions

--- src/RaceKart.java
+++ src/RaceKart.java
@@ -3,8 +3,31 @@
 
 public class RaceKart
 {
+   //Constants
+   static final int DIRECTIONS = 16;
+   static final double PI_CARDINAL_RATIO = Math.PI/16;
+   
+   static final int NORTH = 0;
+   static final int NORTH_NORTH_EAST = 1;
+   static final int NORTH_EAST = 2;
+   static final int NORTH_EAST_EAST = 3;
+   static final int EAST = 4;
+   static final int SOUTH_EAST_EAST = 5;
+   static final int SOUTH_EAST = 6;
+   static final int SOUTH_SOUTH_EAST = 7;
+   static final int SOUTH = 8;
+   static final int SOUTH_SOUTH_WEST = 9;
+   static final int SOUTH_WEST = 10;
+   static final int SOUTH_WEST_WEST = 11;
+   static final int WEST = 12;
+   static final int NORTH_WEST_WEST = 13;
+   static final int NORTH_WEST = 14;
+   static final int NORTH_NORTH_WEST = 15;
+
+
    //Attributes that should stay the same
    private String livery;
+   
    private int weight;
    
    //Attributes that should change
