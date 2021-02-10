14d18ff14f44b66ec424591186230c05958ba88f
MegaDarken <joe.swales@googlemail.com>
2021-02-10 14:39:14 +0000

    PacketHandling External Address and Port storage.

--- src/PacketHandling.java
+++ src/PacketHandling.java
@@ -5,36 +5,38 @@ import java.io.*;
 public class PacketHandling 
 {
    static final int SERVER_PORT = 25610;
-   //static final int SERVER_MAX_CONNECTIONS = 8;
+   static final int SERVER_MAX_CONNECTIONS = 8;
    
-   //static final int MAX_CONNECTIONS = 1;
+   static final int MAX_CONNECTIONS = 1;
    
    
    private DatagramSocket socket;
    
    private DatagramPacket currentPacket;
    
-   //private String[] connectionAddress;
-   //private String[] connectionPort;
+   private String[] connectionAddress;
+   private String[] connectionPort;
    
    
    public PacketHandling(boolean isServer)
    {
       try
 	  	{
+      
+      //Based on 
       if (isServer)
       {
          socket = new DatagramSocket(SERVER_PORT);
          
-         //connectionAddress = new String[SERVER_MAX_CONNECTIONS];
-         //connectionPort = new String[SERVER_MAX_CONNECTIONS];
+         connectionAddress = new String[SERVER_MAX_CONNECTIONS];
+         connectionPort = new String[SERVER_MAX_CONNECTIONS];
       }
       else
       {
          socket = new DatagramSocket();
          
-         //connectionAddress = new String[MAX_CONNECTIONS];
-         //connectionPort = new String[MAX_CONNECTIONS];
+         connectionAddress = new String[MAX_CONNECTIONS];
+         connectionPort = new String[MAX_CONNECTIONS];
       }
       }
 		catch( UnknownHostException e )
