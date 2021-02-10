5c4c4ac19d9013eadf6222516976a04ab919dc7b
MegaDarken <joe.swales@googlemail.com>
2021-02-10 14:27:17 +0000

    PacketHandling Socket and Packet attributes

--- src/PacketHandling.java
+++ src/PacketHandling.java
@@ -4,12 +4,24 @@ import java.io.*;
 
 public class PacketHandling 
 {
+   static final int SERVER_PORT = 25610;
+   
+   private DatagramSocket socket;
+   
+   private DatagramPacket currentPacket;
    
    
-   
-   public PacketHandling()
+   public PacketHandling(boolian isServer)
    {
-   
+      if (isServer)
+      {
+         socket = new DatagramSocket(SERVER_PORT);
+      }
+      else
+      {
+         socket = new DatagramSocket();
+      }
+      
    }
    
 }
