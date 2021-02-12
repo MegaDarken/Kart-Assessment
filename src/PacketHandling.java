2935ddaf9fce435660b3f2b79e22981b075a18d9
MegaDarken <joe.swales@googlemail.com>
2021-02-12 14:38:59 +0000

    PacketHandling added Functional? Send and Receive Methods.

--- src/PacketHandling.java
+++ src/PacketHandling.java
@@ -9,13 +9,14 @@ public class PacketHandling
    
    static final int MAX_CONNECTIONS = 1;
    
+   static final String RESPONCE_STRING = "Received";
    
    private DatagramSocket socket;
    
    private DatagramPacket currentPacket;
    
-   private String[] connectionAddress;
-   private String[] connectionPort;
+   private InetAddress[] connectionAddress;
+   private int[] connectionPort;
    
    
    public PacketHandling(boolean isServer)
@@ -28,16 +29,163 @@ public class PacketHandling
       {
          socket = new DatagramSocket(SERVER_PORT);
          
-         connectionAddress = new String[SERVER_MAX_CONNECTIONS];
-         connectionPort = new String[SERVER_MAX_CONNECTIONS];
+         connectionAddress = new InetAddress[SERVER_MAX_CONNECTIONS];
+         connectionPort = new int[SERVER_MAX_CONNECTIONS];
       }
       else
       {
          socket = new DatagramSocket();
          
-         connectionAddress = new String[MAX_CONNECTIONS];
-         connectionPort = new String[MAX_CONNECTIONS];
+         connectionAddress = new InetAddress[MAX_CONNECTIONS];
+         connectionPort = new int[MAX_CONNECTIONS];
       }
+      }
+		//catch( UnknownHostException e )
+		//{
+			//System.err.println ("UnknownHostException: " + e );
+		//}
+		catch( IOException e )
+		{
+			System.err.println ("IOException: " + e );
+		}
+      
+   }
+   
+   private boolean SendPacket(String dataSent, int hostIndex)
+   {
+      try
+		{
+			// Create a message to send using a UDP packet
+			ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+			PrintStream pout = new PrintStream( bytesOut );
+			pout.print(dataSent);
+			
+			// Get contents of message as an array of bytes
+			byte[] bytesArray = bytesOut.toByteArray();
+			
+			// Create a datagram packet containing the byte array
+			DatagramPacket packet = new DatagramPacket( bytesArray, bytesArray.length );
+			 
+			
+         			
+			// Address packet to sender
+			packet.setAddress(this.connectionAddress[hostIndex]);
+			packet.setPort(this.connectionPort[hostIndex]);
+			
+			// Send the packet - no guarantee  of delivery
+			socket.send( packet );
+			
+			//System.out.println ("Packet sent!" );
+         
+         //RESPONCE
+         packet = new DatagramPacket( new byte[256], 256 );
+         
+         //Receive packet information for responce
+			socket.receive( packet );
+         
+         int senderPort = packet.getPort();
+         InetAddress senderAddress = packet.getAddress();
+         
+         System.out.println ("\nData from " + senderAddress + ":" + senderPort);
+         
+         ByteArrayInputStream bytesIn= new ByteArrayInputStream( packet.getData());
+         
+         String dataReceived = "";
+         
+         for (int i=0; i < packet.getLength(); i++)
+         { 
+            int data = bytesIn.read();
+            
+            if(data == -1 ) 
+            {
+               break;
+            }
+            else
+            {
+               //System.out.print( (char) data);
+               
+               dataReceived += (char) data;
+            }
+         } 
+         
+         //Calculate if received correctly
+         return (this.connectionAddress[hostIndex] == senderAddress && this.connectionPort[hostIndex] == senderPort) && dataReceived == RESPONCE_STRING;
+         
+		}
+		catch( UnknownHostException e )
+		{
+			System.err.println ("UnknownHostException: " + e );
+		}
+		catch( IOException e )
+		{
+			System.err.println ("IOException: " + e );
+		}
+            
+      return false;
+   }
+   
+   
+   private String ReceivePacket()
+   {
+   
+   
+      try
+      {
+         // Create a datagram packet for the byte array
+			DatagramPacket packet = new DatagramPacket( new byte[256], 256 );
+			 
+         //Receive packet information
+			socket.receive( packet );
+         
+         int senderPort = packet.getPort();
+         InetAddress senderAddress = packet.getAddress();
+         
+         System.out.println ("\nData from " + senderAddress + ":" + senderPort);
+         //System.out.println ("Sender address " + senderAddress)
+         //InputStream pin = packet.getData(); // further processing
+         
+         ByteArrayInputStream bytesIn= new ByteArrayInputStream( packet.getData());
+         
+         String dataReceived = "";
+         
+         for (int i=0; i < packet.getLength(); i++)
+         { 
+            int data = bytesIn.read();
+            
+            if(data == -1 ) 
+            {
+               break;
+            }
+            else
+            {
+               //System.out.print( (char) data);
+               
+               dataReceived += (char) data;
+            }
+         } 
+         
+         //Is this data valid?
+         
+         
+         // Create a message to send using a UDP packet
+			ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+			PrintStream pout = new PrintStream( bytesOut );
+			pout.print(RESPONCE_STRING);
+			
+			// Get contents of message as an array of bytes
+			byte[] bytesArray = bytesOut.toByteArray();
+			
+			// Create a datagram packet containing the response array
+			packet = new DatagramPacket( bytesArray, bytesArray.length );
+         
+         // Address packet to sender
+			packet.setAddress( senderAddress );
+			
+			// Set port number to 2000
+			packet.setPort( senderPort );
+         
+         socket.send( packet );
+         
       }
 		catch( UnknownHostException e )
 		{
@@ -48,8 +196,6 @@ public class PacketHandling
 			System.err.println ("IOException: " + e );
 		}
       
+      return null;
    }
-   
-   
-   
 }
