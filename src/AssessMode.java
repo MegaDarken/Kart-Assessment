c1b269aeaf95cdd37cb607dfc792a90de7685ed8
MegaDarken <joe.swales@googlemail.com>
2021-02-15 22:51:58 +0000

    AssessMode added AsServer attribute.

--- src/AssessMode.java
+++ src/AssessMode.java
@@ -8,12 +8,18 @@ class AssessMode
 
    static final String PROGRAM_NAME = "Kart Assignment";
 
+   //Vars
+   static boolean AsServer;
+   
+   //
+
+
    public static void main(String[] args)
    {
       System.out.println("Starting " + PROGRAM_NAME);
       
       //Determine running mode
-      boolean asServer = Arrays.asList(args).contains(ARGUMENT_SERVER);
+      AsServer = Arrays.asList(args).contains(ARGUMENT_SERVER);
       
       
    }
