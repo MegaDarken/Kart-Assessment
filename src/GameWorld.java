a17e2188c707524ccbcddf964522fd74a2399733
MegaDarken <joe.swales@googlemail.com>
2021-02-15 22:48:04 +0000

    Game World, Classes can't be static.

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -3,13 +3,16 @@
 
 public static class GameWorld
 {
+   //Constants
+   private final int MAXIMUM_RACEKARTS = 4;
+
 
    //Game Objects
    private RaceKart[] Karts;
    
    public GameWorld()
    {
-   
+      
    }
    
    
