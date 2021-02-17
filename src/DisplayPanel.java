4e5197b2f65009403bfd1bdf4370a042d572e87d
MegaDarken <joe.swales@googlemail.com>
2021-02-17 23:50:16 +0000

    DisplayPanel added inputKeyMatrix changed by KeyBinds

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -6,7 +6,9 @@ import javax.swing.*;
 public class DisplayPanel extends JPanel implements ActionListener
 {
    //Constants
-   private final int MAXIMUM_IMAGE_ICONS = 64;
+   //private final int TICKS_PER_SECOND = 30;
+   
+   private final int MAXIMUM_IMAGE_ICONS = 64;//16 Karts * 4 Liverys
    private final int MAXIMUM_IMAGE_LABELS = 16;//Arbitary Magic
    
    private final int windowStartingPositionX = 640;
@@ -16,6 +18,7 @@ public class DisplayPanel extends JPanel implements ActionListener
    
    private final int KART_DIRECTIONS = 16;
    
+   //Image(s)
    private final String IMAGE_DIRECTORY = "./Graphics/";
    private final String IMAGE_KART_FILENAME = "Kart";
    private final String IMAGE_FILE_EXTENSION = ".jpg";
@@ -25,6 +28,21 @@ public class DisplayPanel extends JPanel implements ActionListener
    private final String IMAGE_LIVERY_BLUE = "Blue";
    private final String IMAGE_LIVERY_BOT = "Bot";
    
+   //Input Related
+   private final int INPUT_KEY_MATRIX_SIZE = 4;
+   
+   private final int INPUT_FORWARD = 0;
+   private final int INPUT_BACKWARD = 1;
+   private final int INPUT_LEFT = 2;
+   private final int INPUT_RIGHT = 3;
+   
+   private final byte INPUT_ON_VALUE = 127;
+   private final byte INPUT_OFF_VALUE = 0;
+   
+   //Attributes
+   
+   //input
+   private byte[] inputKeyMatrix;
    
    //Window Elements
    private ImageIcon[] imageIcons;
@@ -54,6 +72,8 @@ public class DisplayPanel extends JPanel implements ActionListener
         LoadKartImages();
         
         //Keybinds
+        inputKeyMatrix = new byte[INPUT_KEY_MATRIX_SIZE];
+        
         JPanel framePanel = (JPanel) frame.getContentPane();
         
         InputMap frameInputMap = framePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
@@ -65,12 +85,19 @@ public class DisplayPanel extends JPanel implements ActionListener
         frameInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false), "moveLeft");
         frameInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false), "moveRight");
         
+        frameInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, true), "stopForwards");
+        frameInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, true), "stopBackwards");
+        frameInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true), "stopLeft");
+        frameInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true), "stopRight");
+        
+        //Key press
         frameActionMap.put("moveForwards", new AbstractAction()
         {
          @Override
          public void actionPerformed(ActionEvent e)
          {  
-            actionForwards();
+            inputKeyMatrix[INPUT_FORWARD] = INPUT_ON_VALUE;
+            //actionForwards();
          }
         });
         
@@ -79,7 +106,8 @@ public class DisplayPanel extends JPanel implements ActionListener
          @Override
          public void actionPerformed(ActionEvent e)
          {  
-            actionBackwards();
+            inputKeyMatrix[INPUT_BACKWARD] = INPUT_ON_VALUE;
+            //actionBackwards();
          }
         });
         
@@ -88,7 +116,8 @@ public class DisplayPanel extends JPanel implements ActionListener
          @Override
          public void actionPerformed(ActionEvent e)
          {  
-            actionLeft();
+            inputKeyMatrix[INPUT_LEFT] = INPUT_ON_VALUE;
+            //actionLeft();
          }
         });
         
@@ -97,11 +126,51 @@ public class DisplayPanel extends JPanel implements ActionListener
          @Override
          public void actionPerformed(ActionEvent e)
          {  
-            actionRight();
+            inputKeyMatrix[INPUT_RIGHT] = INPUT_ON_VALUE;
+            //actionRight();
          }
         });
         
+        //Key Release
+        frameActionMap.put("stopForwards", new AbstractAction()
+        {
+         @Override
+         public void actionPerformed(ActionEvent e)
+         {  
+            inputKeyMatrix[INPUT_FORWARD] = INPUT_OFF_VALUE;
+         }
+        });
         
+        frameActionMap.put("stopBackwards", new AbstractAction()
+        {
+         @Override
+         public void actionPerformed(ActionEvent e)
+         {  
+            inputKeyMatrix[INPUT_BACKWARD] = INPUT_OFF_VALUE;
+         }
+        });
+        
+        frameActionMap.put("stopLeft", new AbstractAction()
+        {
+         @Override
+         public void actionPerformed(ActionEvent e)
+         {  
+            inputKeyMatrix[INPUT_LEFT] = INPUT_OFF_VALUE;
+         }
+        });
+        
+        frameActionMap.put("stopRight", new AbstractAction()
+        {
+         @Override
+         public void actionPerformed(ActionEvent e)
+         {  
+            inputKeyMatrix[INPUT_RIGHT] = INPUT_OFF_VALUE;
+         }
+        });
+        
+        //Timer
+        //timer = new Timer(1000 / TICKS_PER_SECOND, this);
+        //timer.start();
    }
    
    
@@ -118,26 +187,26 @@ public class DisplayPanel extends JPanel implements ActionListener
    }
    
    
-   //Action Functions
-   private void actionForwards()
-   {
-   
-   }
-   
-   private void actionBackwards()
-   {
-   
-   }
-   
-   private void actionLeft()
-   {
-   
-   }
-   
-   private void actionRight()
-   {
-   
-   }
+   // //Action Functions
+//    private void actionForwards()
+//    {
+//    
+//    }
+//    
+//    private void actionBackwards()
+//    {
+//    
+//    }
+//    
+//    private void actionLeft()
+//    {
+//    
+//    }
+//    
+//    private void actionRight()
+//    {
+//    
+//    }
 
    //Loading
    private void LoadKartImages()
@@ -183,5 +252,5 @@ public class DisplayPanel extends JPanel implements ActionListener
    {
       return imageIcons[SelectKartImageIndex(currentKart)];
    }
-   
+
 }
