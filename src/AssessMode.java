514f8eb02adf4cec662662ab95456314097889fe
MegaDarken <joe.swales@googlemail.com>
2021-02-22 00:21:31 +0000

    AssessMode Functions as JFrame

--- src/AssessMode.java
+++ src/AssessMode.java
@@ -1,19 +1,46 @@
 //File: AssessMode.java
 import java.util.*;
+import javax.swing.*;
+import java.awt.*;
 
-class AssessMode
+public class AssessMode extends JFrame
 {
    //Constants
    static final String ARGUMENT_SERVER = "-server";
 
    static final String PROGRAM_NAME = "Kart Assignment";
 
-   //Vars
+   static final int WINDOW_X_START = 100;
+   static final int WINDOW_Y_START = 100;
+   
+   static final int WINDOW_WIDTH = 500;
+   static final int WINDOW_HEIGHT = 500;
+   
+   //Static Vars
    static boolean AsServer;
    
    static GameWorld world;
    
-   //
+   //Vars
+   private DisplayPanel panel;
+
+   //Constructor
+   public AssessMode()
+   {
+      setTitle(PROGRAM_NAME);               // JFrame title
+   	setBounds(WINDOW_X_START, WINDOW_Y_START, WINDOW_WIDTH, WINDOW_HEIGHT);        // JFrame location within screen: Position xleft, ydown, width, height
+   	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   
+   	Container cp = getContentPane();   // Need Content Pane so can add to JFrame       
+   	cp.setLayout(null);                // Suppress default layout
+   
+   	panel = new DisplayPanel();              // Create panel containing interface (could just use more than one panel)
+   	panel.setBounds(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);      // Location within JFrame x, y, w, h
+   
+   	cp.add(panel);	                     // Add the Panel to the JFrame
+
+   }
+
 
 
    public static void main(String[] args)
@@ -27,10 +54,18 @@ class AssessMode
       
       if (!AsServer)
       {
-      //Launch Window
-      DisplayPanel window = new DisplayPanel();
-      window.setVisible(true);
+         System.out.println("Client, Opening game window...");
       
+         //Launch Window
+         AssessMode window = new AssessMode();
+         window.setVisible(true);
+      
+      }
+      else
+      {
+         System.out.println("Server, Starting...");
+         
+         
       }
    }
 }
