c5d6e3ca8b091c73d9a88188f26be6ac461b2747
MegaDarken <joe.swales@googlemail.com>
2021-02-23 22:35:42 +0000

    DisplayPanel timer moves karts

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -192,6 +192,7 @@ public class DisplayPanel extends JPanel implements ActionListener
          if(e.getSource() == timer)
          {
             //handleImageAnimation();
+            AssessMode.world.MoveKarts();
             
             //paintComponent();
             
