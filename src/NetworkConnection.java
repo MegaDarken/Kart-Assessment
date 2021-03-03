75a7b68633f03d0eb960320b1e06d49a89a7bf8d
MegaDarken <joe.swales@googlemail.com>
2021-03-03 11:19:35 +0000

    NetworkConnection attribute getters and setters

--- src/NetworkConnection.java
+++ src/NetworkConnection.java
@@ -1,5 +1,7 @@
 //File: NetworkConnection.java
 import java.net.*;
+//import java.lang.*;
+//import java.util.concurrent.*;
 
 class NetworkConnection
 {
@@ -20,4 +22,25 @@ class NetworkConnection
 //          System.out.println("ERR: Unable to resolve name and address");
 //       }
    }
+   
+   public InetAddress GetHostAddress()
+   {
+      return this.hostAddress;
+   }
+   
+   public void SetHostAddress(InetAddress hostAddress)
+   {
+      this.hostAddress = hostAddress;
+   }
+   
+   public int GetHostPort()
+   {
+      return this.hostPort;
+   }
+   
+   public void SetHostPort(int hostPort)
+   {
+      this.hostPort = hostPort;
+   }
+   
 }
