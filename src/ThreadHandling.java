495883090405082069ce5e18777174c8eff66177
MegaDarken <joe.swales@googlemail.com>
2021-03-03 11:08:50 +0000

    ThreadHandling class added

---
+++ src/ThreadHandling.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+import java.util.concurrent.*;
+
+public class ThreadHandling
+{
+
+   private final int THREAD_COUNT = 3;
+
+   public static void OpenThreads()
+   {
+      //Executor
+      ExecutorService threadExecutor = Executors.newFixedThreadPool(THREAD_COUNT);
+      
+      //Runnable worldThread = new 
+      //Runnable graphicsThread = new 
+      //Runnable connectionThread = new 
+      
+      //threadExecutor.execute(worldThread);
+      //threadExecutor.execute(graphicsThread);
+      //threadExecutor.execute(connectionThread);
+
+   }
+
+
+
+{
