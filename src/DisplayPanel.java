ca69bd73df887059d4fdd8edb166c288b0b1a5f9
MegaDarken <joe.swales@googlemail.com>
2021-03-09 21:45:29 +0000

    GUI seperated out and threaded.

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -8,7 +8,7 @@ public class DisplayPanel extends JPanel implements ActionListener
    //Constants
    private final int TICKS_PER_SECOND = 30;
    
-   private final int MAXIMUM_IMAGE_ICONS = 64;//16 Karts * 4 Liverys
+   //private final int MAXIMUM_IMAGE_ICONS = 64;//16 Karts * 4 Liverys
    private final int MAXIMUM_IMAGE_LABELS = 16;//Arbitary Magic
    
    private final int windowStartingPositionX = 640;
@@ -16,20 +16,7 @@ public class DisplayPanel extends JPanel implements ActionListener
    private final int windowStartingSizeX = 640;
    private final int windowStartingSizeY = 480;
    
-   private final int KART_DIRECTIONS = 16;
-   
-   private final int KART_IMAGE_WIDTH = 50;
-   private final int KART_IMAGE_HEIGHT = 50;
-   
-   //Image(s)
-   private final String IMAGE_DIRECTORY = "./Graphics/";
-   private final String IMAGE_KART_FILENAME = "Kart";
-   private final String IMAGE_FILE_EXTENSION = ".jpg";
-   
-   private final String IMAGE_LIVERY_RED = "Red";
-   private final String IMAGE_LIVERY_GREEN = "Green";
-   private final String IMAGE_LIVERY_BLUE = "Blue";
-   private final String IMAGE_LIVERY_BOT = "Bot";
+
    
    //Input Related
    private final int INPUT_KEY_MATRIX_SIZE = 4;
@@ -43,13 +30,12 @@ public class DisplayPanel extends JPanel implements ActionListener
    private final byte INPUT_OFF_VALUE = 0;
    
    //Attributes
-      
+   private GUIHandling guiHandler;
    
    //input
    private byte[] inputKeyMatrix;
    
    //Window Elements
-   private ImageIcon[] imageIcons;
    
    private JFrame frame;
    private JLabel[] imageLabels;
@@ -76,16 +62,14 @@ public class DisplayPanel extends JPanel implements ActionListener
         //frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         
         //Initalize images
-        imageIcons = new ImageIcon[MAXIMUM_IMAGE_ICONS];
+        //imageIcons = new ImageIcon[MAXIMUM_IMAGE_ICONS];
         imageLabels = new JLabel[MAXIMUM_IMAGE_LABELS];
         
-        
-        //Load Resources
-        LoadKartImages();
-        
+        guiHandler = new GUIHandling(imageLabels);
+                
         for (int i = 0; i < MAXIMUM_IMAGE_LABELS; i++)
         {
-            imageLabels[i] = new JLabel(imageIcons[i]);
+            imageLabels[i] = new JLabel();//imageIcons[i]
             
             add(imageLabels[i]);
         }
@@ -167,10 +151,12 @@ public class DisplayPanel extends JPanel implements ActionListener
          {
             //handleImageAnimation();
             Thread worldThread = new Thread(AssessMode.world);
-            worldThread.start();//AssessMode.world.MoveKarts();
-            
+                       
             //paintComponent();
-            UpdateKartImages(AssessMode.world.GetKarts());
+            Thread guiThread = new Thread(guiHandler);//UpdateKartImages(AssessMode.world.GetKarts())
+            
+            worldThread.start();//AssessMode.world.MoveKarts();
+            guiThread.start();
             
             repaint();
          }
@@ -217,59 +203,6 @@ public class DisplayPanel extends JPanel implements ActionListener
       }
   };
 
-   //Loading
-   private void LoadKartImages()
-   {
-      for (int i = 0; i < KART_DIRECTIONS; i++)
-      {
-         imageIcons[i] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_RED + i + IMAGE_FILE_EXTENSION));
-         imageIcons[i + (KART_DIRECTIONS)] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_GREEN + i + IMAGE_FILE_EXTENSION));
-         imageIcons[i + (KART_DIRECTIONS * 2)] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_BLUE + i + IMAGE_FILE_EXTENSION));
-         imageIcons[i + (KART_DIRECTIONS * 3)] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_BOT + i + IMAGE_FILE_EXTENSION));
-      }
-   }
    
-   private int SelectKartImageIndex(RaceKart currentKart)
-   {
-      if (currentKart != null)
-      {
-         //Kart Direction
-         int index = currentKart.GetCardinalDirection();
-         
-         //Which Livery
-         if (currentKart.Livery().equals(IMAGE_LIVERY_GREEN))
-         {
-            index += (KART_DIRECTIONS);
-         }
-         else if (currentKart.Livery().equals(IMAGE_LIVERY_BLUE))
-         {
-            index += (KART_DIRECTIONS * 2);
-         }
-         else if (currentKart.Livery().equals(IMAGE_LIVERY_BOT))
-         {
-            index += (KART_DIRECTIONS * 3);
-         }
-         
-         //System.out.println(index);
-         return index;
-      }
-      
-      return -1;
-   }
-   
-   private ImageIcon SelectKartImage(RaceKart currentKart)
-   {
-      return imageIcons[SelectKartImageIndex(currentKart)];
-   }
-
-   public void UpdateKartImages(RaceKart[] Karts)
-   {
-      for(int i = 0; i < Karts.length; i++)
-      {
-         imageLabels[i].setIcon(SelectKartImage(Karts[i]));
-      
-         imageLabels[i].setBounds(Math.round(Karts[i].X()), Math.round(Karts[i].Y()), KART_IMAGE_WIDTH, KART_IMAGE_HEIGHT);
-      }
-   }
 
 }
---
+++ src/GUIHandling.java
@@ -0,0 +1,100 @@
+//File: GUIHandling.java
+import javax.swing.*;
+
+class GUIHandling implements Runnable
+{
+   //Constants
+   private final int MAXIMUM_IMAGE_ICONS = 64;//16 Karts * 4 Liverys
+   
+   private final int KART_DIRECTIONS = 16;
+      
+   private final int KART_IMAGE_WIDTH = 50;
+   private final int KART_IMAGE_HEIGHT = 50;
+   
+   //Image(s)
+   private final String IMAGE_DIRECTORY = "./Graphics/";
+   private final String IMAGE_KART_FILENAME = "Kart";
+   private final String IMAGE_FILE_EXTENSION = ".jpg";
+   
+   private final String IMAGE_LIVERY_RED = "Red";
+   private final String IMAGE_LIVERY_GREEN = "Green";
+   private final String IMAGE_LIVERY_BLUE = "Blue";
+   private final String IMAGE_LIVERY_BOT = "Bot";
+
+   //Attributes
+   private ImageIcon[] imageIcons;
+   private JLabel[] imageLabels;
+
+   public GUIHandling(JLabel[] imageLabels)
+   {
+      this.imageIcons = new ImageIcon[MAXIMUM_IMAGE_ICONS];
+      
+      this.imageLabels = imageLabels;
+      
+      //Load Resources
+      LoadKartImages();
+      
+      
+   }
+
+   //Loading
+   private void LoadKartImages()
+   {
+      for (int i = 0; i < KART_DIRECTIONS; i++)
+      {
+         imageIcons[i] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_RED + i + IMAGE_FILE_EXTENSION));
+         imageIcons[i + (KART_DIRECTIONS)] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_GREEN + i + IMAGE_FILE_EXTENSION));
+         imageIcons[i + (KART_DIRECTIONS * 2)] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_BLUE + i + IMAGE_FILE_EXTENSION));
+         imageIcons[i + (KART_DIRECTIONS * 3)] = new ImageIcon(getClass().getResource(IMAGE_DIRECTORY + IMAGE_KART_FILENAME + IMAGE_LIVERY_BOT + i + IMAGE_FILE_EXTENSION));
+      }
+   }
+   
+   private int SelectKartImageIndex(RaceKart currentKart)
+   {
+      if (currentKart != null)
+      {
+         //Kart Direction
+         int index = currentKart.GetCardinalDirection();
+         
+         //Which Livery
+         if (currentKart.Livery().equals(IMAGE_LIVERY_GREEN))
+         {
+            index += (KART_DIRECTIONS);
+         }
+         else if (currentKart.Livery().equals(IMAGE_LIVERY_BLUE))
+         {
+            index += (KART_DIRECTIONS * 2);
+         }
+         else if (currentKart.Livery().equals(IMAGE_LIVERY_BOT))
+         {
+            index += (KART_DIRECTIONS * 3);
+         }
+         
+         //System.out.println(index);
+         return index;
+      }
+      
+      return -1;
+   }
+   
+   private ImageIcon SelectKartImage(RaceKart currentKart)
+   {
+      return imageIcons[SelectKartImageIndex(currentKart)];
+   }
+
+   public void UpdateKartImages(RaceKart[] Karts)
+   {
+      for(int i = 0; i < Karts.length; i++)
+      {
+         imageLabels[i].setIcon(SelectKartImage(Karts[i]));
+      
+         imageLabels[i].setBounds(Math.round(Karts[i].X()), Math.round(Karts[i].Y()), KART_IMAGE_WIDTH, KART_IMAGE_HEIGHT);
+      }
+   }
+
+   public void run()
+   {
+      UpdateKartImages(AssessMode.world.GetKarts());
+      
+   }
+}
