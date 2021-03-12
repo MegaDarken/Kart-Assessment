22e65aca172418262831cc8b57f21e6829a4b64c
MegaDarken <joe.swales@googlemail.com>
2021-03-12 01:47:37 +0000

    AssessMode args changed to expect Address and Port

--- src/AssessMode.java
+++ src/AssessMode.java
@@ -2,6 +2,7 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.net.*;
 
 public class AssessMode extends JFrame
 {
@@ -14,6 +15,8 @@ public class AssessMode extends JFrame
    static boolean AsServer;
    
    static GameWorld world;
+   
+   static PacketHandling connectionHandle;
    
    //Vars
    static private ClientFrame frame;
@@ -32,26 +35,86 @@ public class AssessMode extends JFrame
       System.out.println("Starting Assessment Client");
       
       //Determine running mode
-      AsServer = Arrays.asList(args).contains(ARGUMENT_SERVER);
+      //AsServer = Arrays.asList(args).contains(ARGUMENT_SERVER);
       
-      world = new GameWorld();
+      //Get Server address
+      InetAddress serverAddress = null;
+      int serverPort = -1;
       
-      world.SetClientKart(1);
-      
-      if (!AsServer)
+      //Address arg?
+      if (args.length > 0)
       {
-         System.out.println("Client, Opening game window...");
-      
-         //Launch Window
-         frame = new ClientFrame();
-         frame.setVisible(true);
-      
+         try
+         {
+            serverAddress = InetAddress.getByName(args[0]);
+            
+            //Port arg?
+            if (args.length > 1)
+            {
+               serverPort = Integer.parseInt(args[1]);
+               
+               connectionHandle = new PacketHandling(serverAddress, serverPort);
+            }
+            else
+            {
+               connectionHandle = new PacketHandling(serverAddress);
+            }
+         }
+         catch (UnknownHostException e)
+         {
+            System.out.println("Unknown host:" + args[0]);
+         }
+         catch (Exception e)
+         {
+            System.out.println(e);
+         }
       }
       else
       {
-         System.out.println("Server, Starting...");
+         //No Address...
          
+         //Request from user
+         Scanner scan = new Scanner(System.in);
+         String userInput = "";
          
+         try
+         {
+            System.out.println("Server Address:");
+            userInput = scan.nextLine();  // Read user input
+            
+            serverAddress = InetAddress.getByName(userInput);
+            
+            connectionHandle = new PacketHandling(serverAddress);
+         }
+         catch (UnknownHostException e)
+         {
+            System.out.println("Unknown host:" + userInput);
+         }
+         catch (Exception e)
+         {
+            System.out.println(e);
+         }
+         
+
       }
+      
+      //connectionHandle = new PacketHandling();
+      
+      world = new GameWorld();
+      
+      world.SetClientKart(1);//DEBUG
+      
+      
+      
+      //if (!AsServer)
+      //{
+      System.out.println("Client, Opening game window...");
+   
+      //Launch Window
+      frame = new ClientFrame();
+      frame.setVisible(true);
+      
+      //}
+      
    }
 }
