219e08b1dea9ca5817477f9676760a77b9792f60
MegaDarken <joe.swales@googlemail.com>
2021-04-15 22:54:15 +0100

    Threads for world and graphics to be called.

--- src/ThreadHandling.java
+++ src/ThreadHandling.java
@@ -6,17 +6,17 @@ public class ThreadHandling
 
    private final static int THREAD_COUNT = 3;
 
-   public static void OpenThreads()
+   public static void OpenThreads(GUIHandling guiHandler)
    {
       //Executor
       ExecutorService threadExecutor = Executors.newFixedThreadPool(THREAD_COUNT);
       
-      //Runnable worldThread = new 
-      //Runnable graphicsThread = new 
+      Runnable worldThread = new Thread(AssessMode.world);
+      Runnable graphicsThread = new Thread(guiHandler);
       //Runnable connectionThread = new 
       
-      //threadExecutor.execute(worldThread);
-      //threadExecutor.execute(graphicsThread);
+      threadExecutor.execute(worldThread);
+      threadExecutor.execute(graphicsThread);
       //threadExecutor.execute(connectionThread);
 
       threadExecutor.shutdown();
